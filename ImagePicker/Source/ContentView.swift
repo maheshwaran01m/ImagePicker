d562931562fd1ff802079757ef938bb5876de350
MAHESHWARAN <maheshwaran@eam360.com>
2024-04-27 08:08:55 +0530

    Source  - Initial Commit

---
+++ ImagePicker/Source/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ImagePicker
+//
+//  Created by MAHESHWARAN on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+// MARK: - Preview
+
+#Preview {
+  ContentView()
+}
