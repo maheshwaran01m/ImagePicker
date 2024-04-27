47fde38fae18285e9d86b04ef601ed677a01e532
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-27 08:06:51 +0530

    Initial Commit

---
+++ ImagePicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImagePicker
+//
+//  Created by MAHESHWARAN on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImagePicker/ImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerApp.swift
+//  ImagePicker
+//
+//  Created by MAHESHWARAN on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
